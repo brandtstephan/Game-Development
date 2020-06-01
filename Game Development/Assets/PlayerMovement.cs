1bd3d51c646e6161534269b586c1f916a361ae1a
stephan <stephan_000@hotmail.com>
2020-06-01 19:11:37 +0200

    Character Control + Unity project
    Creacion de scripts para el movimiento y control del personaje + Sunnyland Free Assets

---
+++ Game Development/Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CharacterController2D controller;
+
+    private float horizontalMovement = 0f;
+    public float runSpeed = 40f;
+    private bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump")){
+            jump = true;
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMovement * Time.fixedDeltaTime ,false,jump);
+        jump = false;
+    }
+}
