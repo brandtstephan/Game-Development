265491f163b5cccdafa360e567f052f10d742b20
stephan <stephan_000@hotmail.com>
2020-06-03 16:10:14 +0200

    Weapon, Aim, Bulltes and shooting
    Se agrego scripts para Disparar balas, las balas en si y un script para manipular la direccion en la que la arma esta apuntando en base a la position del cursor del mouse

---
+++ Game Development/Assets/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 20f;
+    public Rigidbody2D rb;
+
+    private float timeToLive = 3f;
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+        Destroy(gameObject, timeToLive);
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collidedObject = collision.GetComponent<GameObject>();
+        if(null != collidedObject && collidedObject.layer != LayerMask.GetMask("Player"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+}
---
+++ Game Development/Assets/WeaponAimMouse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class WeaponAimMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform aimtTransform;
+    private void Awake()
+    {
+        aimtTransform = transform.Find("Aim");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = GetMouseWorldPosition();
+
+        Vector3 aimDirection = (mousePos - transform.position).normalized;
+        float angle = Mathf.Atan2(aimDirection.y,aimDirection.x)*Mathf.Rad2Deg;
+        aimtTransform.eulerAngles = new Vector3(0, 0, angle);
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        vec.z = 0f;
+        return vec;
+    }
+}
