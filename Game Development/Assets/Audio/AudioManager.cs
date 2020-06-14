01ff27512a15b02d735a0954e764f0934e44cecb
stephan <stephan_000@hotmail.com>
2020-06-14 11:08:07 +0200

    Audio
    Added Game Looping Song + Audio Manager

---
+++ Game Development/Assets/Audio/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audio;
+    void Start()
+    {
+        audio.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
