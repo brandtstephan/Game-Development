d03baec38e74a2c08ffe5e4d9b6af37d208a8720
stephan <stephan_000@hotmail.com>
2020-06-20 15:14:40 +0200

    Added Text Mesh Pro
    Added a new package to use good quality text, imported a custom PIXEL font and started working on a scrip that spawns "HIT" texts whenever we hit a target

---
+++ Game Development/Assets/GenericScripts/DamagePopUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DamagePopUp : MonoBehaviour
+{
+    private TextMeshPro textMesh;
+    private void Awake()
+    {
+        textMesh = transform.GetComponent<TextMeshPro>();
+    }
+    public void SetUp(bool isCrit)
+    {
+        
+    }
+}
---
+++ Game Development/Assets/GenericScripts/HitPrefab.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPopUp : MonoBehaviour
+{
+    [SerializeField] private Transform hitPopUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(hitPopUp, Vector3.zero, Quaternion.identity);
+    }
+
+}
